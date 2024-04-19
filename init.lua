61ec252130eba23ecc269c986f449f63d714684f
Nelson Ferreira <nfgferreira@gmail.com>
2024-04-19 08:54:57 -0400

    More structured

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
-require("nfgf.core.options")
-require("nfgf.core.keymaps")
+require("nfgf.core")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
